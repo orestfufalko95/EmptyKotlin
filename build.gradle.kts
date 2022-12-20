33f5ab3f1faabe2032d993fe01aef33bf5bbe952
Orest Fufalko <orestfufalko95@gmail.com>
2022-12-20 21:58:08 +0200

    add task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.eclipse.jgit.api.Git
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        jcenter()
+        google()
+    }
+
+    dependencies {
+        classpath("org.eclipse.jgit:org.eclipse.jgit:5.0.1.201806211838-r")
+    }
+}
 
 plugins {
     kotlin("jvm") version "1.7.20"
@@ -26,4 +40,47 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks {
+
+    register("generateGitRepo") {
+
+        inputs.dir("src")
+        inputs.files("build.gradle.kts", "settings.gradle.kts")
+
+        val gitRepoDir = file("$buildDir/git-repo")
+        outputs.dir(gitRepoDir)
+
+        doLast {
+            delete(gitRepoDir)
+            delete(temporaryDir)
+
+            val bareUri = Git.init().setDirectory(gitRepoDir).setBare(true).call().use { bare ->
+                bare.repository.directory.toURI().toString()
+            }
+
+            Git.cloneRepository().setURI(bareUri).setDirectory(temporaryDir).call().use { clone ->
+                copy {
+                    from(".") {
+                        include("src/**")
+                        include("*.gradle.kts")
+                    }
+                    into(temporaryDir)
+                }
+
+                clone.add()
+                    .addFilepattern("build.gradle.kts")
+                    .addFilepattern("settings.gradle.kts")
+                    .addFilepattern("src")
+                    .call()
+                clone.commit()
+                    .setMessage("Initial import")
+                    .setAuthor("name", "email")
+                    .call()
+
+                clone.push().call()
+            }
+        }
+    }
 }
